d4dddefd49c0ef10672ea33274f4d1adae07798b
Andrey Lutov <andrewcyplakov5@gmail.com>
2023-03-08 00:18:19 +0500

    create second salon page

---
+++ src/pages/salons_page_second/salons_page_second.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './salons_second.css'
+import SalonsBlockSecond from './components/salons_header_block_second'
+
+
+
+function SalonsPageSecond() {
+  return (
+    <div className="App">
+      <header className="salons-second">
+              <SalonsBlockSecond/>
+      </header>
+    </div>
+  )
+}
+
+export default SalonsPageSecond
